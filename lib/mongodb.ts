3db0bb23107fcdb7c7f236b98522b6c54f77eadf
parth108 <108parthnikam@gmail.com>
2023-10-22 16:15:52 +0530

    commit no. 1

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -28,6 +28,7 @@ if (process.env.NODE_ENV === 'development') {
   clientPromise = client.connect()
 }
 
+console.log("nigga just Connected!");
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 export default clientPromise
